c8402bd099bdc59c5a6cd2826b511fa34fedd613
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-05-24 21:34:46 -0300

    Versao final

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import arrow from "../assets/Arrow 21.svg"
+
+export default function Header() {
+    const navigate = useNavigate();
+
+    function handleClick() {
+        navigate("/");
+    }
+
+    return (
+        <H>
+            {window.location.pathname !== "/" && (
+                <button onClick={() => handleClick() } data-test="go-home-header-btn" >
+                    <img src={arrow} alt="backs" />
+                </button>
+            )}
+            CINEFLEX
+        </H>
+    );
+}
+
+const H = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    box-shadow:1px 1px 5px orange;
+
+    button {
+        background-color: transparent;
+        border: none;
+        position: absolute;
+        left: 10px;
+        top: center;
+    }
+`;
